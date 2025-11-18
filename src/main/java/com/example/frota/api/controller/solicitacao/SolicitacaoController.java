b5a02e13d7fdb56ec773107be0798786517ab329
uN1CK <nicolasdomingos149@gmail.com>
2025-11-17 22:26:42 -0300

    refactor: Projeto em estrutura clean arch + DDD simplificado

---
+++ src/main/java/com/example/frota/api/controller/solicitacao/SolicitacaoController.java
@@ -0,0 +1,133 @@
+package com.example.frota.api.controller.solicitacao;
+
+import com.example.frota.application.dto.solicitacao.AtualizacaoSolicitacao;
+import com.example.frota.domain.solicitacao.model.Solicitacao;
+import com.example.frota.domain.solicitacao.mapper.SolicitacaoMapper;
+import com.example.frota.domain.solicitacao.service.SolicitacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.frota.domain.caixa.service.CaixaService;
+import com.example.frota.domain.caminhao.service.CaminhaoService;
+import com.example.frota.domain.produto.service.ProdutoService;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/solicitacao")
+public class SolicitacaoController {
+	
+	@Autowired
+	private SolicitacaoService solicitacaoService;
+	
+	@Autowired
+    private SolicitacaoMapper solicitacaoMapper;
+	
+	@Autowired
+	private CaixaService caixaService;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private CaminhaoService caminhaoService;
+	
+	@GetMapping                 
+	public String carregaPaginaFormulario ( Model model){ 
+		//devolver DTO
+		model.addAttribute("lista", solicitacaoService.procurarTodos());
+	    return "solicitacao/listagem";              
+	} 
+	
+	@GetMapping("/formulario")
+    public String mostrarFormulario(@RequestParam(required = false) Long id, Model model) {
+		AtualizacaoSolicitacao dto;
+        if (id != null) {
+            Solicitacao solicitacao = solicitacaoService.procurarPorId(id)
+                .orElseThrow(() -> new EntityNotFoundException("Solicitação não encontrado"));
+            dto = solicitacaoMapper.toAtualizacaoDto(solicitacao);
+        } else {
+            dto = new AtualizacaoSolicitacao(null, null, null, null, null, null, null, null, null, null, null);
+        }
+        model.addAttribute("solicitacao", dto);
+        model.addAttribute("caixas", caixaService.procurarTodos());
+        model.addAttribute("produtos", produtoService.procurarProdutosSemSolicitacao());
+        model.addAttribute("caminhoes", caminhaoService.procurarTodos());
+        return "solicitacao/formulario";
+    }
+	
+	@GetMapping ("/formulario/{id}")    
+	public String carregaPaginaFormulario (@PathVariable("id") Long id, Model model,
+			RedirectAttributes redirectAttributes) {
+		AtualizacaoSolicitacao dto;
+		try {
+	        model.addAttribute("caixas", caixaService.procurarTodos());
+	        model.addAttribute("produtos", produtoService.procurarTodos());
+	        model.addAttribute("caminhoes", caminhaoService.procurarTodos());
+	        
+			if(id != null) {
+				Solicitacao solicitacao = solicitacaoService.procurarPorId(id)
+						.orElseThrow(() -> new EntityNotFoundException("Solicitação não encontrada"));
+				model.addAttribute("solicitacoes", solicitacaoService.procurarTodos());
+
+				dto = solicitacaoMapper.toAtualizacaoDto(solicitacao);
+				model.addAttribute("solicitacao", dto);
+			}
+			return "solicitacao/formulario";
+		} catch (EntityNotFoundException e) {
+			redirectAttributes.addFlashAttribute("error", e.getMessage());
+			return "redirect:/solicitacao";
+		} catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+	}
+	
+
+	@PostMapping("/salvar")
+    public String salvar(@ModelAttribute("solicitacao") @Valid AtualizacaoSolicitacao dto,
+                        BindingResult result,
+                        RedirectAttributes redirectAttributes,
+                        Model model) {
+		if (result.hasErrors()) {
+	        model.addAttribute("solicitacoes", solicitacaoService.procurarTodos());
+	        return "solicitacao/formulario";
+	    }
+	    try {
+	        Solicitacao solicitacaoSalva = solicitacaoService.salvarOuAtualizar(dto);
+	        String mensagem = dto.id() != null 
+	            ? "Solicitação atualizada com sucesso!"
+	            : "Solicitação criada com sucesso!";
+	        redirectAttributes.addFlashAttribute("message", mensagem);
+	        return "redirect:/solicitacao";
+	    } catch (EntityNotFoundException e) {
+	        redirectAttributes.addFlashAttribute("error", e.getMessage());
+	        return "redirect:/solicitacao/formulario" + (dto.id() != null ? "?id=" + dto.id() : "");
+	    } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+	}
+	
+	@GetMapping("/delete/{id}")
+	@Transactional
+	public String deleteTutorial(@PathVariable("id") Long id, Model model, RedirectAttributes redirectAttributes) {
+		try {
+			solicitacaoService.apagarPorId(id);
+			redirectAttributes.addFlashAttribute("message", "A solicitação " + id + " foi apagada!");
+		} catch (Exception e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+		}
+		return "redirect:/solicitacao";
+	}
+	
+}
